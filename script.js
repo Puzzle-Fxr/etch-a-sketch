56050ab5126bbc92e4b11b5c757d96b8f2ec92b3
Puzzle-Fxr <caldwellavogo@gmail.com>
2023-07-29 23:02:58 +0000

    add fuction to create divs in sketch area

--- script.js
+++ script.js
@@ -3,21 +3,47 @@ const sketch = document.querySelector('.sketch');
 const setting = document.querySelector('.setting');
 
 
-const baseH = sketch.getAttribute("width");
-console.log(baseH);
 
-
-function setSquares (h1, w1) {
+function sixteenSquares (h1, w1) {
     let total = h1*w1;
-    console.log(total);
     for (let x=0; x<total; x++){
         const square = document.createElement('div');
         square.classList.add('square');
-        square.style.height=total + "px";
-        square.style.width=total + "px";
+        square.style.height= "8.84mm";
+        square.style.width= "8.84mm";
         square.style.borderStyle = "solid";
+        square.style.borderWidth = "0.1mm";
         sketch.appendChild(square);
     }
 };
 
-//setSquares(8, 8);
+
+function thirtyTwoSquares (h1, w1) {
+    let total = h1*w1;
+    for (let x=0; x<total; x++){
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.height= "4.152mm";
+        square.style.width= "4.152mm";
+        square.style.borderStyle = "solid";
+        square.style.borderWidth = "0.1mm";
+        sketch.appendChild(square);
+    }
+};
+
+function sixtyFourSquares (h1, w1) {
+    let total = h1*w1;
+    for (let x=0; x<total; x++){
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.height= "1.811mm";
+        square.style.width= "1.811mm";
+        square.style.borderStyle = "solid";
+        square.style.borderWidth = "0.1mm";
+        sketch.appendChild(square);
+    }
+};
+
+//sixteenSquares(16, 16);
+//thirtyTwoSquares(32, 32);
+//sixtyFourSquares(64, 64);
